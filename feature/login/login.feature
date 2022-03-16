d5b18dfe153b28c4a412ccae3abf6dcdcde37d68
Muthupandij26 <muthupandij26@gmail.com>
2022-03-16 18:21:20 +0530

    installed cucumber

---
+++ feature/login/login.feature
@@ -0,0 +1,4 @@
+Feature: login details
+
+Scenario: Fill out the form 
+When Now go to the facebook page
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,19 @@
 const { test, expect } = require('@playwright/test');
 const { Constant } = require('../support/constants');
 const { BaseAction } = require('../support/baseAction');
-
-test('my test', async ({ page }) => {
-//   await page.goto(Constant.BASE_URL);
-  const homepage = new BaseAction(page);
-  await homepage.goto(Constant.BASE_URL);
+const { When } = require('@cucumber/cucumber');
 
 
-  await page.close();
-});
+When('Now go to the facebook page', () => {
+  test('my test', async ({ page }) => {
+    //   await page.goto(Constant.BASE_URL);
+      const homepage = new BaseAction(page);
+      await homepage.goto(Constant.BASE_URL);
+      await page.fill('//input[@id="email"]', "check");
+      await page.fill('//input[@id="pass"]', 'password');
+    
+    await page.pause();
+      await page.close();
+    });
+})
+
